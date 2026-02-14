8999ef510973212fcd70a05f9aaea6bb7b7bc179
ruvarvel <ruvave@alumnatflorida.es>
2026-02-14 18:04:19 +0100

    Arreglando dockerfile

--- symfony/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "=== INICIANDO MIGRACIONES ==="
-php bin/console doctrine:migrations:migrate --no-interaction -vvv
-echo "=== MIGRACIONES COMPLETADAS ==="
-echo "=== INICIANDO SERVIDOR ==="
-php -S 0.0.0.0:8080 -t public
