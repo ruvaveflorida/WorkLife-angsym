b878e2111d64225db77f09392a36ab2af49f3ca1
ruvarvel <ruvave@alumnatflorida.es>
2026-02-14 15:59:58 +0100

    Editando index.php

--- symfony/public/index.php
+++ symfony/public/index.php
@@ -4,6 +4,9 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+var_dump($_ENV['APP_ENV'], $_ENV['APP_DEBUG']);
+exit;
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
