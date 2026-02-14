135d375cac3da2981fa02655e7762236965ee172
ruvarvel <ruvave@alumnatflorida.es>
2026-02-14 17:59:48 +0100

    Add migration logs

--- symfony/start.sh
+++ symfony/start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-php bin/console doctrine:migrations:migrate --no-interaction
+echo "=== INICIANDO MIGRACIONES ==="
+php bin/console doctrine:migrations:migrate --no-interaction -vvv
+echo "=== MIGRACIONES COMPLETADAS ==="
+echo "=== INICIANDO SERVIDOR ==="
 php -S 0.0.0.0:8080 -t public
